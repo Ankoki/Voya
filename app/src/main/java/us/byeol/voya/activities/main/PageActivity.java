d0926babab4aebdb216657e3d32b2cf3d8da6434
Ella <ellamcvh@gmail.com>
2025-01-20 04:28:42 +0000

    Not 100% sure exactly what I've done in this update.

--- app/src/main/java/us/byeol/voya/activities/main/PageActivity.java
+++ app/src/main/java/us/byeol/voya/activities/main/PageActivity.java
@@ -5,21 +5,20 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 
 import us.byeol.voya.R;
 import us.byeol.voya.api.Book;
 import us.byeol.voya.api.Page;
-import us.byeol.voya.misc.Log;
 import us.byeol.voya.misc.Misc;
 import us.byeol.voya.misc.popup.PopUp;
 import us.byeol.voya.web.IOHandler;
@@ -41,6 +40,13 @@ public class PageActivity extends AppCompatActivity {
     }
 
     private CoordinatorLayout coordinator;
+    private TextView pageTitle;
+    private ImageView pagePhoto;
+    private TextView pageContent;
+    private ImageButton previousPage;
+    private TextView pageCount;
+    private ImageButton editPage;
+    private ImageButton nextPage;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -62,6 +68,19 @@ public class PageActivity extends AppCompatActivity {
             this.startActivity(new Intent(this.getBaseContext(), HomeActivity.class));
             return;
         }
+        this.displayPage(book, index);
+    }
+
+    /**
+     * Edit the current page shown to stop the faff of having
+     * to create a new activity every time a page is turned.
+     * Editing the contents is more performance friendly.
+     *
+     * @param book the book the page belongs too.
+     * @param index the index of the page.
+     */
+    public void displayPage(Book book, int index) {
+        this.setContentView(R.layout.activity_page);
         ImageButton backButton = this.findViewById(R.id.back_button);
         backButton.setOnClickListener(event -> this.startActivity(new Intent(this.getBaseContext(), HomeActivity.class)));
         TextView bookTitle = this.findViewById(R.id.book_title);
@@ -69,21 +88,42 @@ public class PageActivity extends AppCompatActivity {
         Map<Integer, Page> pages = book.getPages();
         if (pages.isEmpty() || !pages.containsKey(index)) {
             PopUp.instance.showText(this.coordinator, "No content, add content.", PopUp.Length.LENGTH_LONG);
-            return;
         } else {
             Page page = pages.get(index);
             Bitmap photo = page.getPagePhoto(); // No it won't, thanks to the above checks for if the book is valid.
-            ImageView pagePhoto = this.findViewById(R.id.page_photo);
-            pagePhoto.setImageDrawable(null);
+            this.pagePhoto = this.findViewById(R.id.page_photo);
+            this.pagePhoto.setImageDrawable(null);
             if (photo != null)
                 pagePhoto.setImageBitmap(photo);
             String title = page.getTitle();
-            TextView pageTitle = this.findViewById(R.id.page_title);
-            pageTitle.setText(title);
+            this.pageTitle = this.findViewById(R.id.page_title);
+            this.pageTitle.setText(title);
             String content = page.getContent();
             TextView pageContent = this.findViewById(R.id.page_content);
-            pageContent.setText(content);
+            if (!content.isEmpty())
+                pageContent.setText(content);
+            this.previousPage = this.findViewById(R.id.previous_page);
+            this.previousPage.setOnClickListener(event -> {
+                if (book.getPages().size() <= (index - 1))
+                    this.displayPage(book, index - 1);
+                else
+                    PopUp.instance.showText(this.coordinator, "This is the first page.", PopUp.Length.LENGTH_SHORT);
+            });
+            this.pageCount = this.findViewById(R.id.page_count);
+            this.pageCount.setText(index /* + "/" + book.getPages().size() */); // Maybe add the comparison. Might look bad.
+            // TODO set the text styling for PageActivity#pageCount.
+            this.editPage = this.findViewById(R.id.edit_button);
+            this.editPage.setOnClickListener(event -> {
+                // TODO
+            });
+            this.nextPage = this.findViewById(R.id.next_page);
+            this.nextPage.setOnClickListener(event -> {
+                if (book.getPages().size() > (index + 1))
+                    return;
+                this.displayPage(book, index + 1);
+            });
         }
+
     }
 
 }
